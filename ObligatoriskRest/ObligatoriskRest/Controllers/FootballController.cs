3e6f932b7c8394dfb5fb955445e158a219d76f92
mads815g <74011672+mads815g@users.noreply.github.com>
2021-09-28 14:04:34 +0200

    Add files via upload

---
+++ ObligatoriskRest/ObligatoriskRest/Controllers/FootballController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Football;
+using ObligatoriskRest.Manager;
+
+namespace ObligatoriskRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballController : ControllerBase
+    {
+        private readonly FootballManager _manager = new FootballManager();
+
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ ObligatoriskRest/ObligatoriskRest/Manager/FootballManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Football;
+
+namespace ObligatoriskRest.Manager
+{
+    public class FootballManager
+    {
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
+        {
+            new FootballPlayer("Mads", 200, 2),
+            new FootballPlayer ("Peter", 100, 6),
+            new FootballPlayer ("Jakob", 300, 9),
+            new FootballPlayer ("Niels", 400, 4)
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(FootballPlayer => FootballPlayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newFootballPlayer)
+        {
+            Data.Add(newFootballPlayer);
+            return newFootballPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer FootballPlayer = Data.Find(FootballPlayer1 => FootballPlayer1.Id == id);
+            if (FootballPlayer == null) return null;
+            Data.Remove(FootballPlayer);
+            return FootballPlayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer FootballPlayer = Data.Find(FootballPlayer1 => FootballPlayer1.Id == id);
+            if (FootballPlayer == null) return null;
+            FootballPlayer.Name = updates.Name;
+            FootballPlayer.Price = updates.Price;
+            FootballPlayer.ShirtNumber = updates.ShirtNumber;
+            return FootballPlayer;
+        }
+    }
+}
